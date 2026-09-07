d360ef265bb65c78850c5fa0478f7b8769b37c21
savi saluwadana <82082138+savisaluwadana@users.noreply.github.com>
2026-09-07 23:10:50 +0530

    Register unified calendar API

--- services/api/main.go
+++ services/api/main.go
@@ -66,6 +66,7 @@ func main() {
 	a.registerSessionRoutes(mux)
 	a.registerSecretRoutes(mux)
 	a.registerCampaignRoutes(mux)
+	a.registerCalendarRoutes(mux)
 
 	addr := envOr("DEVRELOS_HTTP_ADDR", ":8080")
 	core := a.withAuth(mux)
